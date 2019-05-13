f99e4c8b79bc0563d2f7a73bcbd1bbaa002b1273
Marcel Widmer <marzelwidmer@gmail.com>
2019-05-13 18:46:47 +0200

    Impl. Locator

---
+++ src/main/kotlin/ch/keepcalm/kotlingeode/locator/LocatorApplication.kt
@@ -0,0 +1,23 @@
+package ch.keepcalm.kotlingeode.locator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.data.gemfire.config.annotation.CacheServerApplication
+import org.springframework.data.gemfire.config.annotation.EnableLocator
+import org.springframework.data.gemfire.config.annotation.EnableManager
+
+@CacheServerApplication(locators = "localhost[10334]")
+@SpringBootApplication
+class LocatorApplication
+
+@Configuration
+@EnableLocator
+@EnableManager(start = true)
+@Profile("!clustered")
+internal class LonerConfiguration
+
+fun main(args: Array<String>) {
+    runApplication<LocatorApplication>(*args)
+}
